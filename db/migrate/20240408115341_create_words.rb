f43eb1e811a112902d02ddd404e64e0b2075c80e
W1ldr <dewillribeiro@gmail.com>
2024-04-15 10:15:11 +0100

    fix: column name

--- db/migrate/20240408115341_create_words.rb
+++ db/migrate/20240408115341_create_words.rb
@@ -1,7 +1,7 @@
 class CreateWords < ActiveRecord::Migration[7.1]
   def change
     create_table :words do |t|
-      t.string :w, null: false
+      t.string :word, null: false
       t.string :syllabic_division
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_115357) do
   end
 
   create_table "words", force: :cascade do |t|
-    t.string "w", null: false
+    t.string "word", null: false
     t.string "syllabic_division"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
