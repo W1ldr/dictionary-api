8e393f3b927dd834439227b53182c83958f5e947
Wilder Ribeiro <67587576+W1ldr@users.noreply.github.com>
2024-04-20 09:43:31 +0100

    feat: list by letter

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,12 +1,22 @@
 class WordsController < ApplicationController
   before_action :word_info, only: [:show]
-  before_action :search_params, only: [:index]
+  before_action :search_params, only: [:index, :list_by_letter]
+  before_action :get_letter, only: [:list_by_letter]
 
   # GET /words
   def index
     @words = Word.order(word: :asc).page(@page).per(@per_page).pluck(:word)
 
     render json: @words
+  end
+
+  def list_by_letter
+    if @letter.present?
+      @words = Word.where("word like ?", "#{@letter}%").order(word: :asc).page(@page).per(@per_page).pluck(:word)
+      render json: @words
+    else
+      render json: { message: I18n.t("letter_not_found") }, status: :not_found
+    end
   end
 
   # GET /words/:word
@@ -57,6 +67,14 @@ class WordsController < ApplicationController
       end
       if @page.nil? || @page.zero?
         @page = 1
+      end
+    end
+
+    def get_letter
+      @letter = params[:letter]
+      
+       if !@letter.match?(/\A[a-zA-Z]\z/) || @letter.size != 1
+        @letter = nil
       end
     end
 
